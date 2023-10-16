ff1193d2b22d95a0d1919b0bdab169e12494a2bd
junkRatte <saso.tos1@gmail.com>
2023-10-16 11:01:31 +0200

    Deployment

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCY-B7aHW4rRccwMTZJmIQu628D1Rfr_nM",
-  authDomain: "move-software.firebaseapp.com",
-  projectId: "move-software",
-  storageBucket: "move-software.appspot.com",
-  messagingSenderId: "867199092595",
-  appId: "1:867199092595:web:14a290118200fa185c2706",
+  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
+  authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
+  projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
+  storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "process.env.REACT_APP_FIREBASE_APP_ID",
 };
 
 export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const LazyLoad = React.lazy(() => import("./App.jsx"));
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Suspense fallback={<Loader />}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <LazyLoad />
       </BrowserRouter>
     </Suspense>
