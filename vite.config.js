6f9453b1fb1641ee1b73f2d32ec104d23d558026
junkRatte <saso.tos1@gmail.com>
2023-10-18 12:07:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/movesoftware-react/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["/movesoftware-react/assets/index-ea2f8442.js"],
+    },
+  },
 });
