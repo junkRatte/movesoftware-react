9e8057a7d853212a6a8dcc1d7522c8afd184d3b0
junkRatte <saso.tos1@gmail.com>
2023-10-18 16:40:08 +0200

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/movesoftware-react/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["/movesoftware-react/assets/index-ea2f8442.js"],
-    },
-  },
 });
