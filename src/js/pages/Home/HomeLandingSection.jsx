800ffc8cd9a9b71f4996d20e05b7791583f63bd1
junkRatte <saso.tos1@gmail.com>
2023-10-19 11:31:51 +0200

    Update HomeLandingSection.jsx

--- src/js/pages/Home/HomeLandingSection.jsx
+++ src/js/pages/Home/HomeLandingSection.jsx
@@ -7,6 +7,7 @@ function HomeLandingSection() {
   return (
     <section className="homepage__landing">
       <div className="content-container">
+        <h1>test</h1>
         <h1>Move is the perfect development tool</h1>
         <p>
           We have created software for each employee of your team to help them
