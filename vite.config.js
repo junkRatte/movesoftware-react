2475bac6c711234e47194e437a0158ca438ebfc3
junkRatte <saso.tos1@gmail.com>
2023-10-19 09:31:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/movesoftware-react/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 });
