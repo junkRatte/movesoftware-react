0351d8d2f918a3868c24ce1e6fc6101972540411
junkRatte <saso.tos1@gmail.com>
2023-10-20 13:35:47 +0200

    Deployment test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/movesoftware-react/",
   plugins: [react()],
+  define: {
+    "import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY": `"${process.env.VITE_REACT_APP_FIREBASE_API_KEY}"`,
+    "import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN": `"${process.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN}"`,
+    "import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID": `"${process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID}"`,
+    "import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET": `"${process.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET}"`,
+    "import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID": `"${process.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID}"`,
+    "import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID": `"${process.env.VITE_REACT_APP_FIREBASE_APP_ID}"`,
+  },
 });
