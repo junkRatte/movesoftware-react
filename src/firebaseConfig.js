443b03f4bf59920926a3af4b1a070ba5edf92089
junkRatte <saso.tos1@gmail.com>
2023-10-26 14:36:22 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: String(import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY),
-  authDomain: String(import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN),
-  projectId: String(import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID),
-  storageBucket: String(import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET),
-  messagingSenderId: String(
-    import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-  ),
-  appId: String(import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID),
+  apiKey: process.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.VITE_REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
