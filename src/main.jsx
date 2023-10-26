a2283c00c12b88afe5c18b6e2659fbb9a6773b36
junkRatte <saso.tos1@gmail.com>
2023-10-26 11:39:33 +0200

    Deployment test

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "./firebaseConfig";
 
 import { Suspense } from "react";
 import Loader from "./js/components/Loader.jsx";
