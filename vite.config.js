c8e2c994e4353e8d42424a2a0c57fda5211d47a5
junkRatte <saso.tos1@gmail.com>
2023-11-09 13:08:44 +0100

    Deployment with rollup options

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import includePaths from "rollup-plugin-includepaths";
 
 export default defineConfig({
   base: "/movesoftware-react/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [includePaths({ paths: ["./"] })],
+    },
+  },
 });
