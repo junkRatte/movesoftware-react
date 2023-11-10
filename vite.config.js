2add7fed6ee8a9e15800834b1b5332ef1029d219
junkRatte <saso.tos1@gmail.com>
2023-11-10 10:55:44 +0100

    Updated vite config and workflow

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import includePaths from "rollup-plugin-includepaths";
 
 export default defineConfig({
   base: "/movesoftware-react/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [includePaths({ paths: ["./"] })],
-    },
-  },
 });
